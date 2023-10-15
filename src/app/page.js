cb88bb14cebd8b4312e24186d679e27b6402eac9
jmpeoples <jmpeoples.88@gmail.com>
2023-10-15 13:40:58 -0500

    Add Button

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import { TableLayout } from './tableLayout'
-
+import Button from './button'
 export default function Home() {
   return (
     <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+      <Button />
       <TableLayout />
     </main>
   )
